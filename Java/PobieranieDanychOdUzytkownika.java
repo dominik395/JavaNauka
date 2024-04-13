560a411eff54273bc8d802e1a323b776152c4f8f
dominik395 <chojnowski.dominik@wp.pl>
2024-04-13 18:05:35 +0200

    pobieranie danych od uzytkownia w konsoli

---
+++ Java/PobieranieDanychOdUzytkownika.java
@@ -0,0 +1,16 @@
+package Java;
+import java.util.Scanner;
+
+public class PobieranieDanychOdUzytkownika {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        System.out.println("Hi " + name + "! Your age is " + age);
+    }
+}
