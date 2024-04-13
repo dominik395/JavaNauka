2f05d0f7ba37f6c18ffdb506cf568b4d284ce8db
dominik395 <chojnowski.dominik@wp.pl>
2024-04-13 20:07:16 +0200

    Dodanie wiecej opcji

--- Java/PobieranieDanychOdUzytkownika.java
+++ Java/PobieranieDanychOdUzytkownika.java
@@ -2,16 +2,20 @@ package Java;
 import java.util.Scanner;
 
 public class PobieranieDanychOdUzytkownika {
-    public static void main(String[] args) {
+    static public void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("What is your name?");
         String name = scanner.nextLine();
 
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
+        System.out.println("What is your surname?");
+        String surname = scanner.nextLine();
 
-        System.out.println("Hi " + name + "! Your age is " + age);
+        System.out.println("How old are you?");
+        int age = scanner.nextInt(); 
+
+        System.out.println("Name: " + name + "\nSunrname: " + surname + "\nAge: " + age);
+        
         scanner.close();
     }
 }
